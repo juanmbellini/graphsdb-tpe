0b4fd75e9f11d739bd04dff436b261400c35d505
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-11-18 16:13:57 -0300

    Create an empty java project
    - will be used to call janusgraph

---
+++ janus/janus-java/src/main/java/ar/edu/itba/nosql/tpe/janus_java/App.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.nosql.tpe.janus_java;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
