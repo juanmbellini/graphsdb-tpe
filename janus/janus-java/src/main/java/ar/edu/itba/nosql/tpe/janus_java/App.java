557a8dc3df7efc8352d935aea20dc76b1e9c3e1d
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-11-18 16:59:10 -0300

    Add janus cql and logback dependency
    - Also implement a count vertex query

--- janus/janus-java/src/main/java/ar/edu/itba/nosql/tpe/janus_java/App.java
+++ janus/janus-java/src/main/java/ar/edu/itba/nosql/tpe/janus_java/App.java
@@ -1,10 +1,32 @@
 package ar.edu.itba.nosql.tpe.janus_java;
 
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.janusgraph.core.JanusGraph;
+import org.janusgraph.core.JanusGraphFactory;
+
 /**
- * Hello world!
+ * Main class.
  */
 public class App {
+
+    /**
+     * Entry point.
+     *
+     * @param args Program arguments.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final double before = System.currentTimeMillis();
+        final String configFile = "/Users/jbellini/Dropbox/ITBA/Electivas/NoSQL/TPE/janus/provided_data.properties";
+        final JanusGraph graph = JanusGraphFactory.open(configFile);
+        final GraphTraversal<Vertex, Vertex> t = graph.traversal().V();
+        int count = 0;
+        while (t.hasNext()) {
+            count++;
+            t.next();
+        }
+        System.out.println("Vertex amount: " + count);
+        graph.close();
+        System.out.println("Time elapsed: " + (System.currentTimeMillis() - before) + " ms");
     }
 }
