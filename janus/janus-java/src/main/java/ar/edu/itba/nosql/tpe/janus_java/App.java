c0c3fb70de33e2d952f110f0829028cee393c1de
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-11-19 17:35:23 -0300

    First approach for janus's query 1

--- janus/janus-java/src/main/java/ar/edu/itba/nosql/tpe/janus_java/App.java
+++ janus/janus-java/src/main/java/ar/edu/itba/nosql/tpe/janus_java/App.java
@@ -1,14 +1,19 @@
 package ar.edu.itba.nosql.tpe.janus_java;
 
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Main class.
  */
 public class App {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(App.class);
 
     /**
      * Entry point.
@@ -16,17 +21,47 @@ public class App {
      * @param args Program arguments.
      */
     public static void main(final String... args) {
-        final double before = System.currentTimeMillis();
-        final String configFile = "/Users/jbellini/Dropbox/ITBA/Electivas/NoSQL/TPE/janus/provided_data.properties";
-        final JanusGraph graph = JanusGraphFactory.open(configFile);
-        final GraphTraversal<Vertex, Vertex> t = graph.traversal().V();
-        int count = 0;
-        while (t.hasNext()) {
-            count++;
-            t.next();
+        final String configFile = "/Users/jbellini/Dropbox/ITBA/Electivas/NoSQL/TPE/janus/dataset-5-100.properties";
+        try (final JanusGraph graph = JanusGraphFactory.open(configFile)) {
+
+            final GraphTraversal<Vertex, ?> result = graph.traversal().V()
+                    .match(
+                            buildMatchForCategory("home", "Home"),
+                            buildMatchForCategory("station", "Station"),
+                            buildMatchForCategory("airport", "Airport"),
+                            __.as("home")
+                                    .out("trajStep")
+                                    .as("station")
+                                    .out("trajStep")
+                                    .as("airport")
+
+                    )
+                    .select("home", "station", "airport")
+                    .by(__.out("isVenue").out("hasCategory").out("subCategoryOf").properties("cattype"));
+
+
+            while (result.hasNext()) {
+                System.out.println(result.next());
+            }
+
+        } catch (Throwable e) {
+            LOGGER.error("An exception was thrown", e);
         }
-        System.out.println("Vertex amount: " + count);
-        graph.close();
-        System.out.println("Time elapsed: " + (System.currentTimeMillis() - before) + " ms");
+    }
+
+    /**
+     * Builds a match {@link org.apache.tinkerpop.gremlin.process.traversal.Traversal} for stops with in a venue
+     * with the given {@code category}, marking the step with the given {@code label}.
+     *
+     * @param label    The label to be given to the step.
+     * @param category The category of the venue.
+     * @return The created match traversal.
+     */
+    private static GraphTraversal<?, Vertex> buildMatchForCategory(final String label, final String category) {
+        return __.as(label).hasLabel("Stop")
+                .out("isVenue")
+                .out("hasCategory")
+                .out("subCategoryOf")
+                .has("cattype", category);
     }
 }
